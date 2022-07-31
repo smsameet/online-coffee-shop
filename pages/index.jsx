84643fbb62d111cc2ecb5412115617455fd4e0e8
sameet <sameetmoosavi@yahoo.com>
2022-07-31 21:33:41 +0430

    update index page and added new images

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 const index = () => {
   return (
-    <h1 className='text-4xl text-center'>Hello World</h1>
+    <div className='flex'>
+      <div className='bg-[url("../public/sidebar.jpg")] w-96 lg:block hidden'></div>
+      <div className='bg-[url("../public/slide.jpg")] w-full relative h-96 md:h-screen bg-cover md:bg-center bg-right'></div>
+    </div>
   )
 }
 
