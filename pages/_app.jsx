173f1469b02f5357d6291b784e4549a4e2599fb0
sameet <sameetmoosavi@yahoo.com>
2022-08-01 10:46:05 +0430

    require sidebar to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import Layout from '../components/layout'
+import SideBar from '../components/SideBar'
 import '../styles/globals.css'
 
 const MyApp = ({Component, pageProps}) => {
   return (
     <Layout>
+      <SideBar></SideBar>
       <Component {...pageProps}></Component>
     </Layout>
   )
