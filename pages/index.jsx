b6a4df224d27468d660c9467fcaeba2eb2e1b857
sameet <sameetmoosavi@yahoo.com>
2022-08-01 10:45:28 +0430

    update index page ( remove sidebar )

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,6 @@
 const index = () => {
   return (
-    <div className='flex'>
-      <div className='bg-[url("../public/sidebar.jpg")] w-96 lg:block hidden'></div>
-      <div className='bg-[url("../public/slide.jpg")] w-full relative h-96 md:h-screen bg-cover md:bg-center bg-right'></div>
-    </div>
+    <div className='bg-[url("../public/slide.jpg")] flex w-full relative h-96 md:h-screen bg-cover md:bg-center bg-right'></div>
   )
 }
 
