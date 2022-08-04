cafd9167d05a6cae48a225d132fd30b102c3d552
sameet <sameetmoosavi@yahoo.com>
2022-08-04 23:17:02 +0430

    update

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -7,7 +7,7 @@ const SideBar = () => {
     setShowMenu(!showMenu)
   }
   return (
-    <div className='bg-[url("../public/sidebar.jpg")] w-96 lg:block hidden'>
+    <div className='bg-[url("../public/sidebar.jpg")] w-2/12 lg:block hidden'>
       <Link href='/'><a><img src='/logo.png' className='m-auto p-5 border-stone-600 border-b-2' /></a></Link>
       <nav className='text-center text-white'>
         <Link href='/'><a className='block p-4 text-orange-400'>HOME</a></Link>
@@ -19,7 +19,7 @@ const SideBar = () => {
             <Link href='/'><a className='block p-4 hover:text-orange-400' >ERROR 404</a></Link>
           </nav>
         )}
-        <Link href='/'><a className='block p-4 hover:text-orange-400' >MENU</a></Link>
+        <Link href='/menu'><a className='block p-4 hover:text-orange-400' >MENU</a></Link>
         <Link href='/'><a className='block p-4 hover:text-orange-400' >GALLERY</a></Link>
         <Link href='/'><a className='block p-4 hover:text-orange-400' >CONTACT US</a></Link>
         <Link href='/'><a className='block p-4 hover:text-orange-400' >RESERVATION</a></Link>
