79e7039fd902c93010b6afbb731ff1dc4ff6a39c
sameet <sameetmoosavi@yahoo.com>
2022-08-05 23:17:43 +0430

    update menu page div classes

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -1,6 +1,6 @@
 const menu = () => {
   return (
-    <div className='flex w-10/12 h-screen justify-center items-center'>
+    <div className='flex flex-col ml-auto w-10/12 h-screen justify-center items-center'>
       <h1>Hello World!!!</h1>
     </div>
   )
