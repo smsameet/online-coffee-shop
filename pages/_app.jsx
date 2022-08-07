5ab6020c6855b7904d4981edaff12092d12df38c
sameet <sameetmoosavi@yahoo.com>
2022-08-07 13:59:48 +0430

    require footer component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,17 @@
 import Layout from '../components/layout'
 import SideBar from '../components/SideBar'
+import Footer from '../components/footer'
 import '../styles/globals.css'
 
 const MyApp = ({Component, pageProps}) => {
   return (
-    <Layout>
-      <Component {...pageProps}></Component>
-      <SideBar></SideBar>
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps}></Component>
+        <SideBar></SideBar>
+      </Layout>
+      <Footer></Footer>
+    </>
   )
 }
 
